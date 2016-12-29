0f0e1b89a6188e73f570ca7b3e59a33f2f5de472
Micah Bales <micahbales@gmail.com>
2016-12-28 22:55:01 -0500

    add MapBox to project w/ full screen map in index view

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWljYWhiYWxlcyIsImEiOiJjaXg4OTlrNHgwMDAyMnlvNDRleXBrdGNrIn0.d3eUGWL--AriB6n5MXy5TA';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
+    center: [-76.930355, 38.890102], // starting position
+    zoom: 15 // starting zoom
+});
