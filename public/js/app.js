c39841278b413636af00bdf93f3e9d4d2a4e3c1e
Micah Bales <micahbales@gmail.com>
2016-12-29 09:50:54 -0500

    set map position to user location

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,24 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibWljYWhiYWxlcyIsImEiOiJjaXg4OTlrNHgwMDAyMnlvNDRleXBrdGNrIn0.d3eUGWL--AriB6n5MXy5TA';
-var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
-    center: [-76.930355, 38.890102], // starting position
-    zoom: 15 // starting zoom
-});
+
+function newMap(pos) {
+  let crd = pos.coords;
+  let long = crd.longitude;
+  let lat = crd.latitude;
+
+  var map = new mapboxgl.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
+      center: [long, lat], // starting position
+      zoom: 15 // starting zoom
+  });
+}
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+function initializeMap() {
+  navigator.geolocation.getCurrentPosition(newMap, error); // use geolocation to set map's center
+}
+
+initializeMap();
