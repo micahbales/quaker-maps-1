db92f7da3c74b1ab38df21b5765be41267bee868
Micah Bales <micahbales@gmail.com>
2016-12-31 17:20:21 -0500

    refactor for more semantic & object-oriented code (+ comments)

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,44 @@
-function success(pos) {
+// grab coordinate from geolocation and plug them into initMap
+
+function loadMapGeolocation(pos) {
   var crd = pos.coords;
   var lng = crd.longitude;
   var lat = crd.latitude;
 
-  console.log('Your current position is:');
-  console.log('Latitude : ' + crd.latitude);
-  console.log('Longitude: ' + crd.longitude);
-  console.log('More or less ' + crd.accuracy + ' meters.');
-
   initMap(lng, lat);
 };
 
-function error(err) {
+// if there is an error with geolocation
+
+function geolocationError(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
 };
+
+// otherwise, create the map to be displayed
 
 function initMap(userLng, userLat) {
   var userLocation = {lat: userLat, lng: userLng};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
-    center: userLocation || {lat: 38.890102, lng: -76.930355}
+    center: userLocation || {lat: 38.890102, lng: -76.930355} // defaults to washington, dc
   });
+  placeMarkers(map, userLng, userLat);
+}
+
+// and place markers when initMap is triggered
+
+function placeMarkers(map, lng, lat) {
   var marker = new google.maps.Marker({
-    position: userLocation,
+    position: {lat: lat, lng: lng},
     map: map
   });
-}
+};
+
+// get user geolocation and execute the above code
 
 if (navigator.geolocation) {
   var getUserLocation = (function() {
-    navigator.geolocation.getCurrentPosition(success, error);
+    navigator.geolocation.getCurrentPosition(loadMapGeolocation, geolocationError);
   })();
 } else {
   console.log('no geolocation');
