5c7ad641456e90fbeaca03a9f09d7b573426b829
Micah Bales <micahbales@gmail.com>
2017-01-02 13:11:11 -0500

    add infowindows with meeting information

--- public/js/app.js
+++ public/js/app.js
@@ -6,11 +6,11 @@ function initMap() {
 function placeMarkers(map) {
   var bounds = new google.maps.LatLngBounds(); // create boundaries of all markers
   var meetings = $.getJSON("./js/north-america-meetings.json", (json) => {
-  createMarkers(json, bounds);
+  createMarkers(map, json, bounds);
   map.fitBounds(bounds); // zoom and center the map according to all markers placed
 });
 
-function createMarkers(json, bounds) {
+function createMarkers(map, json, bounds) {
   var markers = [];
     for (let i = 0; i < json.length; i++) {
       let lat = Number(json[i].latitude);
@@ -20,12 +20,28 @@ function createMarkers(json, bounds) {
         position: {lat: lat, lng: lng},
         map: map
       });
+      setMarkerInfoWindow(map, marker, json[i]);
       markers.push(marker);
       bounds.extend(markers[i].getPosition()); // expand `bounds` according to the new marker
-      console.log(`Marker${i} latitude: ${markers[i].position.lat()}`);
-      console.log(`Marker${i} longitude: ${markers[i].position.lat()}`);
     }
   };
+}
+
+function setMarkerInfoWindow(map, marker, meetingInfo) {
+  let windowContent = `
+  <h1 id='meeting-name'> ${meetingInfo.name}</h1>
+    <h3>Address:</h3> <em>${meetingInfo.address} ${meetingInfo.city}, ${meetingInfo.state} ${meetingInfo.zip}</em>
+    <h3>Contact:</h3> <em>${meetingInfo.email} ${meetingInfo.phone}</em>
+    <h3>Yearly Meeting:</h3> <em>${meetingInfo.yearlymeeting}</em>
+    <h3>Branch:</h3> <em>${meetingInfo.branch}</em>
+    <h3>Worship Style:</h3> <em>${meetingInfo.worshipstyle}</em>`
+  var infowindow = new google.maps.InfoWindow({
+    content: windowContent
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
 }
 
 initMap();
