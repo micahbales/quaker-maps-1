2f37a26df1c9ac1e3475072a1cf508ac122d57c9
Micah Bales <micahbales@gmail.com>
2017-01-02 11:29:34 -0500

    remove geolocation collection

--- public/js/app.js
+++ public/js/app.js
@@ -1,54 +1,29 @@
-// grab coordinate from geolocation and plug them into initMap
-
-function loadMapGeolocation(pos) {
-  var crd = pos.coords;
-  var lng = crd.longitude;
-  var lat = crd.latitude;
-
-  initMap(lng, lat);
-};
-
-// if there is an error with geolocation
-
-function geolocationError(err) {
-  console.warn('ERROR(' + err.code + '): ' + err.message);
-};
-
-// otherwise, create the map to be displayed
-
-function initMap(userLng, userLat) {
-  var userLocation = {lat: userLat, lng: userLng};
+function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: userLocation || {lat: 48.21158, lng: -99.59531} // defaults to geographical center of north america
+    center: {lat: 48.21158, lng: -99.59531} // defaults to geographical center of north america
   });
   placeMarkers(map);
 }
 
-// and place markers when initMap is triggered
-
 function placeMarkers(map) {
   var meetings = $.getJSON("./js/north-america-meetings.json", (json) => {
+  var markers = [];
 
     for (let i = 0; i < json.length; i++) {
 
       let lat = Number(json[i].latitude);
       let lng = Number(json[i].longitude);
 
-      var marker = new google.maps.Marker({
+      let marker = new google.maps.Marker({
         position: {lat: lat, lng: lng},
         map: map
       });
+      markers.push(marker);
+      console.log(`Marker${i} latitude: ${markers[i].position.lat()}`);
+      console.log(`Marker${i} longitude: ${markers[i].position.lat()}`);
     }
   });
 };
 
-// get user geolocation and execute the above code
-
-if (navigator.geolocation) {
-  var getUserLocation = (function() {
-    navigator.geolocation.getCurrentPosition(loadMapGeolocation, geolocationError);
-  })();
-} else {
-  console.log('no geolocation');
-}
+initMap();
