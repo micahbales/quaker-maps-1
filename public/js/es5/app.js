6153212fc2e5dfc9952efd05dfa931e52e7865d3
Micah Bales <micahbales@gmail.com>
2017-01-21 22:53:43 -0500

    initMap() creates initial map; afterward, repopulate markers on same map

--- public/js/es5/app.js
+++ public/js/es5/app.js
@@ -1,155 +1,164 @@
 "use strict";
 
-var prevSearchResults = [];
+$(document).ready(function () {
 
-function initMap() {
-  var searchLimits = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : searchLimits || new Object();
-  var allCriteriaMustBeTrue = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : allCriteriaMustBeTrue || true;
+  var prevSearchResults = [];
+  var searchLimits = new Object();
+  var allCriteriaMustBeTrue = true;
+  var markers = [];
 
-  // set custom map styles
-  var mapStyles = [{ "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{ "color": "#444444" }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2f2f2" }] }, { "featureType": "poi", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "saturation": -100 }, { "lightness": 45 }] }, { "featureType": "road.highway", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#46bcec" }, { "visibility": "on" }] }];
-  var styledMap = new google.maps.StyledMapType(mapStyles, { name: "QuakerMaps" });
-  // create new map with custom controls
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoomControl: true,
-    mapTypeControl: false,
-    scaleControl: true,
-    streetViewControl: false,
-    fullscreenControl: true,
-    fullscreenControlOptions: {
-      position: google.maps.ControlPosition.RIGHT_BOTTOM
-    }
-  });
-  // assign custom styles to new map
-  map.mapTypes.set('styled_map', styledMap);
-  map.setMapTypeId('styled_map');
-  populateMap(map, searchLimits, allCriteriaMustBeTrue);
-}
-
-function populateMap(map, searchLimits, allCriteriaMustBeTrue) {
-  var bounds = new google.maps.LatLngBounds(); // create boundaries of all markers
-  var meetings = $.getJSON("./js/north-america-meetings.json", function (meetingData) {
-    var filteredMeetingResults = filterMeetingResults(meetingData, searchLimits, allCriteriaMustBeTrue);
-    createMarkers(map, filteredMeetingResults, bounds);
-    map.fitBounds(bounds); // zoom and center the map according to all markers placed
-    var zoomChangeBoundsListener = google.maps.event.addListenerOnce(map, 'bounds_changed', function (event) {
-      // make sure the zoom isn't too tight
-      if (this.getZoom() > 14) {
-        this.setZoom(14);
-      }
-      if (this.getZoom() < 2) {
-        this.setZoom(2);
+  function initMap() {
+    // set custom map styles
+    var mapStyles = [{ "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{ "color": "#444444" }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2f2f2" }] }, { "featureType": "poi", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "saturation": -100 }, { "lightness": 45 }] }, { "featureType": "road.highway", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#46bcec" }, { "visibility": "on" }] }];
+    var styledMap = new google.maps.StyledMapType(mapStyles, { name: "QuakerMaps" });
+    // create new map with custom controls
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoomControl: true,
+      mapTypeControl: false,
+      scaleControl: true,
+      streetViewControl: false,
+      fullscreenControl: true,
+      fullscreenControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
       }
     });
-  });
-}
+    // assign custom styles to new map
+    map.mapTypes.set('styled_map', styledMap);
+    map.setMapTypeId('styled_map');
+    return map;
+  }
 
-function filterMeetingResults(meetingData, searchLimits, allCriteriaMustBeTrue) {
-  var filteredResults = [];
+  function populateMap(map, searchLimits, allCriteriaMustBeTrue) {
+    var bounds = new google.maps.LatLngBounds(); // create boundaries of all markers
+    var meetings = $.getJSON("./js/north-america-meetings.json", function (meetingData) {
+      var filteredMeetingResults = filterMeetingResults(meetingData, searchLimits, allCriteriaMustBeTrue);
+      createMarkers(map, filteredMeetingResults, bounds);
+      map.fitBounds(bounds); // zoom and center the map according to all markers placed
+      var zoomChangeBoundsListener = google.maps.event.addListenerOnce(map, 'bounds_changed', function (event) {
+        // make sure the zoom isn't too tight or wide
+        if (this.getZoom() > 14) {
+          this.setZoom(14);
+        }
+        if (this.getZoom() < 2) {
+          this.setZoom(2);
+        }
+      });
+    });
+  }
 
-  for (var i = 0; i < meetingData.length; i++) {
-    var currentMeeting = meetingData[i];
-    var allCriteriaAreTrue = true;
+  function filterMeetingResults(meetingData, searchLimits, allCriteriaMustBeTrue) {
+    var filteredResults = [];
 
-    if (currentMeeting.latitude && currentMeeting.longitude) {
-      // must check that latitude & longitude are defined, otherwise Google Maps crashes
-      for (var searchKey in searchLimits) {
-        var searchValue = searchLimits[searchKey];
-        var meetingValue = currentMeeting[searchKey];
-        if (!meetingValue || !meetingValue.includes(searchValue)) {
-          allCriteriaAreTrue = false;
-        } else if (!allCriteriaMustBeTrue && meetingValue && meetingValue.includes(searchValue)) {
+    for (var i = 0; i < meetingData.length; i++) {
+      var currentMeeting = meetingData[i];
+      var allCriteriaAreTrue = true;
+
+      if (currentMeeting.latitude && currentMeeting.longitude) {
+        // must check that latitude & longitude are defined, otherwise Google Maps crashes
+        for (var searchKey in searchLimits) {
+          var searchValue = searchLimits[searchKey];
+          var meetingValue = currentMeeting[searchKey];
+
+          if (!meetingValue || !meetingValue.includes(searchValue)) {
+            allCriteriaAreTrue = false;
+          } else if (!allCriteriaMustBeTrue && meetingValue && meetingValue.includes(searchValue)) {
+            filteredResults.push(currentMeeting);
+          }
+        }
+        if (allCriteriaMustBeTrue && allCriteriaAreTrue) {
           filteredResults.push(currentMeeting);
         }
       }
-      if (allCriteriaMustBeTrue && allCriteriaAreTrue) {
-        filteredResults.push(currentMeeting);
+    }
+    if (filteredResults.length > 0) {
+      prevSearchResults = filteredResults;
+      return filteredResults;
+    } else {
+      noResultsAlert();
+      return prevSearchResults;
+    }
+  }
+
+  function noResultsAlert() {
+    var alertContent = "\n    <div class=\"no-results-alert\">\n        <h1>Not Found!</h1>\n        <p>Looks like no meetings meet your search criteria.</p>\n    </div>\n    ";
+
+    $('main').append(alertContent);
+
+    var fadeOutAlert = window.setTimeout(function () {
+      $('.no-results-alert').fadeOut(1500);
+    }, 3000);
+  }
+
+  function createMarkers(map, filteredMeetingResults, bounds) {
+
+    if (filteredMeetingResults) {
+      // remove previous markers from map
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+      }
+      markers = [];
+      // add new markers
+      for (var _i = 0; _i < filteredMeetingResults.length; _i++) {
+        var meetingInfo = filteredMeetingResults[_i];
+        var lat = Number(filteredMeetingResults[_i].latitude);
+        var lng = Number(filteredMeetingResults[_i].longitude);
+
+        var marker = new google.maps.Marker({
+          position: { lat: lat, lng: lng },
+          map: map
+        });
+        setMarkerInfoWindow(map, marker, meetingInfo);
+        markers.push(marker);
+        bounds.extend(markers[_i].getPosition()); // expand `bounds` according to the new marker
       }
     }
-  }
-  if (filteredResults.length > 0) {
-    prevSearchResults = filteredResults;
-    return filteredResults;
-  } else {
-    noResultsAlert();
-    return prevSearchResults;
-  }
-}
-
-function createMarkers(map, filteredMeetingResults, bounds) {
-
-  var markers = [];
-  if (filteredMeetingResults) {
-    for (var i = 0; i < filteredMeetingResults.length; i++) {
-      var meetingInfo = filteredMeetingResults[i];
-      var lat = Number(filteredMeetingResults[i].latitude);
-      var lng = Number(filteredMeetingResults[i].longitude);
-
-      var marker = new google.maps.Marker({
-        position: { lat: lat, lng: lng },
-        map: map
-      });
-      setMarkerInfoWindow(map, marker, meetingInfo);
-      markers.push(marker);
-      bounds.extend(markers[i].getPosition()); // expand `bounds` according to the new marker
-    }
-  }
-};
-
-// so that only one infowindow may be open at a time
-var infoWindow;
-function getInfoWindow() {
-  if (infoWindow == null) {
-    infoWindow = new google.maps.InfoWindow({
-      content: ""
-    });
-  }
-  return infoWindow;
-}
-
-function setMarkerInfoWindow(map, marker, meetingInfo) {
-  var windowContent = "\n  <h1 id='meeting-name'> " + meetingInfo.name + "</h1>\n    <h3>Address:</h3> <em>" + (meetingInfo.address || "") + " " + (meetingInfo.city || "") + ", " + (meetingInfo.state || "") + " " + (meetingInfo.zip || "") + "</em>\n    <h3>Contact:</h3> <em>" + (meetingInfo.email || "") + " " + (meetingInfo.phone || "") + "</em>\n    <h3>Yearly Meeting:</h3> <em>" + (meetingInfo.yearlymeeting || "not affiliated") + "</em>\n    <h3>Branch:</h3> <em>" + (meetingInfo.branch || "not affiliated") + "</em>\n    <h3>Worship Style:</h3> <em>" + (meetingInfo.worshipstyle || "not defined") + "</em>";
-
-  google.maps.event.addListener(marker, 'click', function () {
-    getInfoWindow().setContent(windowContent);
-    getInfoWindow().open(map, this);
-  });
-}
-
-$(document).ready(function () {
-  initMap();
-});
-
-$('.search-button').on('click', function (e) {
-  e.preventDefault();
-
-  var searchLimits = {
-    "state": $('#state').val(),
-    "worshipstyle": $('#worshipstyle').val(),
-    "branch": $('#branch').val(),
-    "yearlymeeting": $('#yearlymeeting').val()
   };
 
-  function processSearchLimits(searchLimits) {
-    for (var key in searchLimits) {
-      if (searchLimits[key] === "none selected") {
-        delete searchLimits[key];
-      }
+  // so that only one infowindow may be open at a time
+  var infoWindow;
+  function getInfoWindow() {
+    if (infoWindow == null) {
+      infoWindow = new google.maps.InfoWindow({
+        content: ""
+      });
     }
-    return searchLimits;
+    return infoWindow;
   }
 
-  searchLimits = processSearchLimits(searchLimits);
+  function setMarkerInfoWindow(map, marker, meetingInfo) {
+    var windowContent = "\n    <h1 id='meeting-name'> " + meetingInfo.name + "</h1>\n      <h3>Address:</h3> <em>" + (meetingInfo.address || "") + " " + (meetingInfo.city || "") + ", " + (meetingInfo.state || "") + " " + (meetingInfo.zip || "") + "</em>\n      <h3>Contact:</h3> <em>" + (meetingInfo.email || "") + " " + (meetingInfo.phone || "") + "</em>\n      <h3>Yearly Meeting:</h3> <em>" + (meetingInfo.yearlymeeting || "not affiliated") + "</em>\n      <h3>Branch:</h3> <em>" + (meetingInfo.branch || "not affiliated") + "</em>\n      <h3>Worship Style:</h3> <em>" + (meetingInfo.worshipstyle || "not defined") + "</em>";
 
-  initMap(searchLimits);
+    google.maps.event.addListener(marker, 'click', function () {
+      getInfoWindow().setContent(windowContent);
+      getInfoWindow().open(map, this);
+    });
+  }
+
+  $('.search-button').on('click', function (e) {
+    e.preventDefault();
+
+    var searchLimits = {
+      "state": $('#state').val(),
+      "worshipstyle": $('#worshipstyle').val(),
+      "branch": $('#branch').val(),
+      "yearlymeeting": $('#yearlymeeting').val()
+    };
+
+    function processSearchLimits(searchLimits) {
+      for (var key in searchLimits) {
+        if (searchLimits[key] === "none selected") {
+          delete searchLimits[key];
+        }
+      }
+      return searchLimits;
+    }
+
+    searchLimits = processSearchLimits(searchLimits);
+
+    populateMap(map, searchLimits, allCriteriaMustBeTrue);
+  });
+
+  // on initial page load
+  var map = initMap();
+  populateMap(map, searchLimits, allCriteriaMustBeTrue);
 });
-
-function noResultsAlert() {
-  var alertContent = "\n  <div class=\"no-results-alert\">\n      <h1>Not Found!</h1>\n      <p>Looks like no meetings meet your search criteria.</p>\n  </div>\n  ";
-
-  $('main').append(alertContent);
-
-  var fadeOutAlert = window.setTimeout(function () {
-    $('.no-results-alert').fadeOut(1500);
-  }, 3000);
-}
--- public/js/es6/app.js
+++ public/js/es6/app.js
@@ -1,159 +1,175 @@
-var prevSearchResults = [];
+$(document).ready(function(){
 
-function initMap(searchLimits = searchLimits || new Object(), allCriteriaMustBeTrue = allCriteriaMustBeTrue || true) {
-  // set custom map styles
-  var mapStyles = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}];
-  var styledMap = new google.maps.StyledMapType(mapStyles, {name : "QuakerMaps"});
-  // create new map with custom controls
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoomControl: true,
-    mapTypeControl: false,
-    scaleControl: true,
-    streetViewControl: false,
-    fullscreenControl: true,
-    fullscreenControlOptions: {
-      position: google.maps.ControlPosition.RIGHT_BOTTOM
-    }
-  });
-  // assign custom styles to new map
-  map.mapTypes.set('styled_map', styledMap);
-  map.setMapTypeId('styled_map');
-  populateMap(map, searchLimits, allCriteriaMustBeTrue);
-}
+  var prevSearchResults = [];
+  var searchLimits = new Object();
+  var allCriteriaMustBeTrue = true;
+  let markers = [];
 
-function populateMap(map, searchLimits, allCriteriaMustBeTrue) {
-  var bounds = new google.maps.LatLngBounds(); // create boundaries of all markers
-  var meetings = $.getJSON("./js/north-america-meetings.json", (meetingData) => {
-    var filteredMeetingResults = filterMeetingResults(meetingData, searchLimits, allCriteriaMustBeTrue);
-    createMarkers(map, filteredMeetingResults, bounds);
-    map.fitBounds(bounds); // zoom and center the map according to all markers placed
-    let zoomChangeBoundsListener = google.maps.event.addListenerOnce(map, 'bounds_changed', function(event) { // make sure the zoom isn't too tight
-        if (this.getZoom() > 14) {
-            this.setZoom(14);
-        }
-        if (this.getZoom() < 2) {
-          this.setZoom(2);
-        }
+  function initMap() {
+    // set custom map styles
+    let mapStyles = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}];
+    let styledMap = new google.maps.StyledMapType(mapStyles, {name : "QuakerMaps"});
+    // create new map with custom controls
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoomControl: true,
+      mapTypeControl: false,
+      scaleControl: true,
+      streetViewControl: false,
+      fullscreenControl: true,
+      fullscreenControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
+      }
     });
-  });
-}
+    // assign custom styles to new map
+    map.mapTypes.set('styled_map', styledMap);
+    map.setMapTypeId('styled_map');
+    return map;
+  }
 
-function filterMeetingResults(meetingData, searchLimits, allCriteriaMustBeTrue) {
-  let filteredResults = [];
+  function populateMap(map, searchLimits, allCriteriaMustBeTrue) {
+    let bounds = new google.maps.LatLngBounds(); // create boundaries of all markers
+    let meetings = $.getJSON("./js/north-america-meetings.json", (meetingData) => {
+      let filteredMeetingResults = filterMeetingResults(meetingData, searchLimits, allCriteriaMustBeTrue);
+      createMarkers(map, filteredMeetingResults, bounds);
+      map.fitBounds(bounds); // zoom and center the map according to all markers placed
+      let zoomChangeBoundsListener = google.maps.event.addListenerOnce(map, 'bounds_changed', function(event) {
+          // make sure the zoom isn't too tight or wide
+          if (this.getZoom() > 14) {
+              this.setZoom(14);
+          }
+          if (this.getZoom() < 2) {
+            this.setZoom(2);
+          }
+      });
+    });
+  }
 
-  for (let i = 0; i < meetingData.length; i++) {
-    let currentMeeting = meetingData[i];
-    let allCriteriaAreTrue = true;
+  function filterMeetingResults(meetingData, searchLimits, allCriteriaMustBeTrue) {
+    let filteredResults = [];
 
-    if (currentMeeting.latitude && currentMeeting.longitude) { // must check that latitude & longitude are defined, otherwise Google Maps crashes
-      for (var searchKey in searchLimits) {
-        var searchValue = searchLimits[searchKey];
-        var meetingValue = currentMeeting[searchKey];
-        if (!meetingValue || !meetingValue.includes(searchValue)) {
-          allCriteriaAreTrue = false;
-        } else if (!allCriteriaMustBeTrue && meetingValue && meetingValue.includes(searchValue)) {
+    for (let i = 0; i < meetingData.length; i++) {
+      let currentMeeting = meetingData[i];
+      let allCriteriaAreTrue = true;
+
+      if (currentMeeting.latitude && currentMeeting.longitude) { // must check that latitude & longitude are defined, otherwise Google Maps crashes
+        for (let searchKey in searchLimits) {
+          let searchValue = searchLimits[searchKey];
+          let meetingValue = currentMeeting[searchKey];
+
+          if (!meetingValue || !meetingValue.includes(searchValue)) {
+            allCriteriaAreTrue = false;
+          } else if (!allCriteriaMustBeTrue && meetingValue && meetingValue.includes(searchValue)) {
+            filteredResults.push(currentMeeting);
+          }
+        }
+        if (allCriteriaMustBeTrue && allCriteriaAreTrue) {
           filteredResults.push(currentMeeting);
         }
       }
-      if (allCriteriaMustBeTrue && allCriteriaAreTrue) {
-        filteredResults.push(currentMeeting);
+    }
+    if (filteredResults.length > 0) {
+      prevSearchResults = filteredResults;
+      return filteredResults;
+    } else {
+      noResultsAlert();
+      return prevSearchResults;
+    }
+  }
+
+  function noResultsAlert() {
+    let alertContent = `
+    <div class="no-results-alert">
+        <h1>Not Found!</h1>
+        <p>Looks like no meetings meet your search criteria.</p>
+    </div>
+    `
+
+    $('main').append(alertContent);
+
+    let fadeOutAlert = window.setTimeout(function(){
+      $('.no-results-alert').fadeOut(1500);
+    }, 3000);
+  }
+
+  function createMarkers(map, filteredMeetingResults, bounds) {
+
+    if (filteredMeetingResults) {
+      // remove previous markers from map
+      for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+      }
+      markers = [];
+      // add new markers
+      for (let i = 0; i < filteredMeetingResults.length; i++) {
+        let meetingInfo = filteredMeetingResults[i];
+        let lat = Number(filteredMeetingResults[i].latitude);
+        let lng = Number(filteredMeetingResults[i].longitude);
+
+        let marker = new google.maps.Marker({
+          position: {lat: lat, lng: lng},
+          map: map
+        });
+        setMarkerInfoWindow(map, marker, meetingInfo);
+        markers.push(marker);
+        bounds.extend(markers[i].getPosition()); // expand `bounds` according to the new marker
       }
     }
-  }
-  if (filteredResults.length > 0) {
-    prevSearchResults = filteredResults;
-    return filteredResults;
-  } else {
-    noResultsAlert();
-    return prevSearchResults;
-  }
-}
+  };
 
-function createMarkers(map, filteredMeetingResults, bounds) {
-
-  var markers = [];
-  if (filteredMeetingResults) {
-    for (let i = 0; i < filteredMeetingResults.length; i++) {
-      let meetingInfo = filteredMeetingResults[i];
-      let lat = Number(filteredMeetingResults[i].latitude);
-      let lng = Number(filteredMeetingResults[i].longitude);
-
-      let marker = new google.maps.Marker({
-        position: {lat: lat, lng: lng},
-        map: map
+  // so that only one infowindow may be open at a time
+  var infoWindow;
+  function getInfoWindow()	{
+    if (infoWindow == null)	{
+      infoWindow = new google.maps.InfoWindow({
+        content: ""
       });
-      setMarkerInfoWindow(map, marker, meetingInfo);
-      markers.push(marker);
-      bounds.extend(markers[i].getPosition()); // expand `bounds` according to the new marker
     }
+    return infoWindow;
   }
-};
 
-// so that only one infowindow may be open at a time
-var infoWindow;
-function getInfoWindow()	{
-  if (infoWindow == null)	{
-    infoWindow = new google.maps.InfoWindow({
-      content: ""
+  function setMarkerInfoWindow(map, marker, meetingInfo) {
+    let windowContent = `
+    <h1 id='meeting-name'> ${meetingInfo.name}</h1>
+      <h3>Address:</h3> <em>${meetingInfo.address || ""} ${meetingInfo.city || ""}, ${meetingInfo.state || ""} ${meetingInfo.zip || ""}</em>
+      <h3>Contact:</h3> <em>${meetingInfo.email || ""} ${meetingInfo.phone || ""}</em>
+      <h3>Yearly Meeting:</h3> <em>${meetingInfo.yearlymeeting || "not affiliated"}</em>
+      <h3>Branch:</h3> <em>${meetingInfo.branch || "not affiliated"}</em>
+      <h3>Worship Style:</h3> <em>${meetingInfo.worshipstyle || "not defined"}</em>`
+
+    google.maps.event.addListener(marker, 'click', function(){
+      getInfoWindow().setContent(windowContent);
+      getInfoWindow().open(map,this);
     });
   }
-  return infoWindow;
-}
 
-function setMarkerInfoWindow(map, marker, meetingInfo) {
-  let windowContent = `
-  <h1 id='meeting-name'> ${meetingInfo.name}</h1>
-    <h3>Address:</h3> <em>${meetingInfo.address || ""} ${meetingInfo.city || ""}, ${meetingInfo.state || ""} ${meetingInfo.zip || ""}</em>
-    <h3>Contact:</h3> <em>${meetingInfo.email || ""} ${meetingInfo.phone || ""}</em>
-    <h3>Yearly Meeting:</h3> <em>${meetingInfo.yearlymeeting || "not affiliated"}</em>
-    <h3>Branch:</h3> <em>${meetingInfo.branch || "not affiliated"}</em>
-    <h3>Worship Style:</h3> <em>${meetingInfo.worshipstyle || "not defined"}</em>`
 
-  google.maps.event.addListener(marker, 'click', function(){
-    getInfoWindow().setContent(windowContent);
-    getInfoWindow().open(map,this);
-  });
-}
+    $('.search-button').on('click', function(e) {
+      e.preventDefault();
 
-$(document).ready(function(){
-  initMap();
-});
-
-$('.search-button').on('click', function(e) {
-  e.preventDefault();
-
-  var searchLimits = {
-    "state": $('#state').val(),
-    "worshipstyle": $('#worshipstyle').val(),
-    "branch": $('#branch').val(),
-    "yearlymeeting": $('#yearlymeeting').val()
-  }
-
-  function processSearchLimits(searchLimits) {
-    for (let key in searchLimits) {
-      if (searchLimits[key] === "none selected") {
-        delete searchLimits[key];
+      let searchLimits = {
+        "state": $('#state').val(),
+        "worshipstyle": $('#worshipstyle').val(),
+        "branch": $('#branch').val(),
+        "yearlymeeting": $('#yearlymeeting').val()
       }
-    }
-    return searchLimits;
-  }
 
-  searchLimits = processSearchLimits(searchLimits);
+      function processSearchLimits(searchLimits) {
+        for (let key in searchLimits) {
+          if (searchLimits[key] === "none selected") {
+            delete searchLimits[key];
+          }
+        }
+        return searchLimits;
+      }
 
-  initMap(searchLimits);
+      searchLimits = processSearchLimits(searchLimits);
+
+      populateMap(map, searchLimits, allCriteriaMustBeTrue);
+
+  });
+
+  // on initial page load
+  const map = initMap();
+  populateMap(map, searchLimits, allCriteriaMustBeTrue);
+
 });
-
-function noResultsAlert() {
-  let alertContent = `
-  <div class="no-results-alert">
-      <h1>Not Found!</h1>
-      <p>Looks like no meetings meet your search criteria.</p>
-  </div>
-  `
-
-  $('main').append(alertContent);
-
-  let fadeOutAlert = window.setTimeout(function(){
-    $('.no-results-alert').fadeOut(1500);
-  }, 3000);
-}
